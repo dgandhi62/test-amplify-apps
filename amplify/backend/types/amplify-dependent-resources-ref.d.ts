389b3a75fce2fe217edbc4d3303866be6cffaa45
dgandhi62 <gandhidhyan@gmail.com>
2025-11-11 18:36:14 -0500

    feat: add storage to app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -18,5 +18,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "dgtestappS3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
